38b691bd343950e968aa9c0cc4452dfbdeb9171f
Yash Pathak <furiousbluffer@gmail.com>
2022-01-31 09:26:02 +0000

    Added a random pyhton file

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-
 print(1)
 print("This is 31st of Jan, 3 PM, DevOps Lab")
-exit()
-
+print("This is 31st of Jan, 3 PM, DevOps Lab")
 print("Adding this line locally")
+exit()
+# Added this line explicitly on GitKraken while resolving conflicts
